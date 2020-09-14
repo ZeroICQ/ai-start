bda4663124483fcb1d50b3143ba8e50679f8e0bf
Alexey Mogilyovkin <zeroicq@gmail.com>
2020-09-14 22:53:49 +1000

    oneR

--- oner.py
+++ oner.py
@@ -5,8 +5,8 @@ from typing import Tuple, List, Dict, Set
 
 def parse_line(line: str) -> Tuple[List[str], str]:
     words = line.split()
-    attributes = list(map(lambda x: x.lower(), words[:-1]))
-    klass = words[-1:][0]
+    attributes = list(map(lambda x: x.lower().strip(), words[:-1]))
+    klass = words[-1:][0].strip()
     return attributes, klass
 
 def predict(preidctor, attrs):
@@ -15,8 +15,8 @@ def predict(preidctor, attrs):
 if __name__ == '__main__':
     input = sys.stdin
     N, K = map(int, input.readline().split())
-    features = list(map(lambda x: x.lower(), input.readline().split()))
-    features_set: Set = set([f.lower() for f in features])
+    features = list(map(lambda x: x.lower().strip(), input.readline().split()))
+    features_set: Set = set([f.lower().strip() for f in features])
     features_len = len(features_set)
     feature_indices_to_name: Dict[int, str] = {i: f for i, f in enumerate(features)}
     feature_name_to_index: Dict[str, int] = {f: i for i, f in enumerate(features)}
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     M = int(input.readline())
     for i in range(M):
         words = input.readline().split()
-        attributes = list(map(lambda x: x.lower(), words))
+        attributes = list(map(lambda x: x.lower().strip(), words))
         test_data.append(attributes)
 
     predictor_tables: Dict[str, Dict[str, Dict[str, int]]] = {}
@@ -49,7 +49,10 @@ if __name__ == '__main__':
 
     for name, predictor in predictor_tables.items():
         for k, kk in predictor.items():
-            predictor_tables[name][k] = max(kk, key=kk.get)
+            if kk.get('0', 0) == kk.get('1', 0):
+                predictor_tables[name][k] = '1'
+            else:
+                predictor_tables[name][k] = max(kk, key=kk.get)
 
     predictor_errors = {}
     for feature, predictor in predictor_tables.items():
@@ -58,10 +61,11 @@ if __name__ == '__main__':
             interesting_feature = attrs[feature_name_to_index[feature]]
             prediction = predictor_tables[feature][interesting_feature]
             wrong_results += 1 if prediction != klass else 0
-        predictor_errors[feature] = (len(train_data) - wrong_results) / len(train_data)
+        predictor_errors[feature] = wrong_results / len(train_data)
 
 
-    best_predictor = max(predictor_errors, key=predictor_errors.get)
+    # best_predictor = max(predictor_errors, key=predictor_errors.get)
+    best_predictor = sorted(predictor_errors.items(), key=lambda x: (x[1], feature_name_to_index[x[0]]))[0][0]
 
     for attrs in test_data:
         interesting_feature = attrs[feature_name_to_index[best_predictor]]
